d7c36323a5303f567e334b342a1f77576d579541
PSK6262 <qkrxotn60@gmail.com>
2026-05-12 12:48:14 +0900

    iter , 중첩반복문

--- files/java/javaStudy/src/Iter/Iter01.java
+++ files/java/javaStudy/src/Iter/Iter01.java
@@ -3,9 +3,23 @@ package Iter;
 public class Iter01 {
 
 	public static void main(String[] args) {
-		
-		for(int i=0;i<10;i++) {
-			System.out.println("밥먹자"+i);
+//		
+//		for(int i=0;i<10;i++) {
+//			System.out.println("밥먹자"+i);
+//		}
+//		
+//		for(int i=1;i<=10;i++){
+//			System.out.println("다이어트 "+i+"일차");
+//		}
+		int sum = 0;
+		for (int n=1;n<=5;n++) {
+			sum = sum + n;
 		}
+		System.out.println(sum);
+		int sum2 = 0;
+		for (int n=1;n<=10;n++) {
+			sum2 = sum2 + n*10;
+		}
+		System.out.println("10,20,30...90,100까지의 합"+sum2);
 	}
 }
