371ac8495e2bdbd4c2b6bcd55273cbb050ca71a3
PSK6262 <qkrxotn60@gmail.com>
2026-05-20 10:44:14 +0900

    class 예제

---
+++ files/java/javaStudy/src/cls/Student.java
@@ -0,0 +1,41 @@
+package cls;
+
+public class Student {
+	
+	private String name;
+	private String phone;
+	private String studentNumber;
+	private int grade;
+	Student(String name,String phone,String studentNumber,int grade){
+		this.setName(name);
+		this.setPhone(phone);
+		this.setStudentNumber(studentNumber);
+		this.setGrade(grade);
+	}
+	Student(){
+	}
+	int getGrade() {
+		return grade;
+	}
+	void setGrade(int grade) {
+		this.grade = grade;
+	}
+	String getStudentNumber() {
+		return studentNumber;
+	}
+	void setStudentNumber(String studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+	String getPhone() {
+		return phone;
+	}
+	void setPhone(String phone) {
+		this.phone = phone;
+	}
+	String getName() {
+		return name;
+	}
+	void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ files/java/javaStudy/src/cls/StudentMain.java
@@ -0,0 +1,19 @@
+package cls;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student a = new Student("A","010-0000-0000","45-44108817",2);
+		System.out.println(a.getGrade());
+		
+		Student stu1 = null;
+		stu1 = new Student();
+		
+		stu1.setName("이름1");
+		System.out.println(stu1.getName());
+		stu1.setGrade(4);
+		System.out.println(stu1.getGrade());
+		stu1.setPhone("010-1010-0101");
+		System.out.println(stu1.getPhone());
+	}
+}
