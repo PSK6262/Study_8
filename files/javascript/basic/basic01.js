ea53d0f9c04e302672927c05c965156356d3f6ef
PSK6262 <qkrxotn60@gmail.com>
2026-06-04 16:14:42 +0900

    javascript 기초문법,연산자,변수,조건문

---
+++ files/javascript/basic/basic01.js
@@ -0,0 +1,30 @@
+
+console.log('basic01.js');
+
+
+// 변수
+// var let const
+
+// let < 일반적
+// var 쓰지마라
+// const 상수
+
+let a = 20;
+let b = 30;
+let c = "안녕하세요";
+let d = '작은따옴표';
+let e = true;
+let f = false;
+
+const g = 30;
+const h = "hello";
+
+console.log(' "안녕" "테스트" ');
+console.log(" '안녕' '테스트' ");
+console.log("안녕 \" ");
+console.log(` 안녕 백틱 테스트 `);
+
+let x = 20;
+let y;
+console.log(x); // 20
+console.log(y); // undefined
