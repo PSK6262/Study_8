f0529907792b7d60cb6666a688e03688e0a0ae14
PSK6262 <qkrxotn60@gmail.com>
2026-06-05 11:33:33 +0900

    object(객체) 설명

---
+++ files/javascript/basic/obj02.js
@@ -0,0 +1,18 @@
+// Object 객체
+// {Key:Value,Key:Value,Key:Value}
+// 자바의 Map이랑 비슷
+let x = 10;
+//let arr = [1,2,3,4,5];
+
+//console.log(arr[2]);
+//console.log(arr);
+// JSON : Javascript Object Notation
+let weatherInfo = {
+    day : '260605',
+    weather : '맑음',
+    temperature : 5,
+    location : '양재동'
+};
+
+console.log(weatherInfo);
+console.log(weatherInfo.day);
