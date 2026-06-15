bb7608a8eafb76b9374f745a84960281f77164a6
PSK6262 <qkrxotn60@gmail.com>
2026-06-15 12:41:42 +0900

    quiz03해답과 quiz폴더 세분화

--- files/react/react-study/src/App.jsx
+++ files/react/react-study/src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Quiz02 from './quiz/Quiz02'
-import Props02 from './components/Props02'
+import Quiz03 from './quiz/quiz03/Quiz03'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +13,8 @@ function App() {
     //<State03/>
     //<Props01/>
     //<Quiz02/>
-    <Props02/>
+    //<Props02/>
+    <Quiz03/>
   )
 }
 
