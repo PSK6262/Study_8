80fa39b47f2ddc96bec9705c30bcb374315a4d0d
PSK6262 <qkrxotn60@gmail.com>
2026-06-15 11:34:45 +0900

    배열state 활용, 컴포넌트 props 전달

--- files/react/react-study/src/App.jsx
+++ files/react/react-study/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Quiz02 from './quiz/Quiz02'
+import Props02 from './components/Props02'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
     //<Quiz01/>
     //<State03/>
     //<Props01/>
-    <Quiz02/>
+    //<Quiz02/>
+    <Props02/>
   )
 }
 
