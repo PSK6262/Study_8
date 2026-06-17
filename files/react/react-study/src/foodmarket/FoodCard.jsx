8b7fb4f21815542fb809260206302a2b6c431ffa
PSK6262 <qkrxotn60@gmail.com>
2026-06-17 12:19:46 +0900

    FoodMarket FoodCard 컴포넌트화,foodsData.js 이미지 경로 추가 , 상세경로 설정시 접근경로

---
+++ files/react/react-study/src/foodmarket/FoodCard.jsx
@@ -0,0 +1,31 @@
+
+import {Button, Navbar , Card} from 'react-bootstrap';
+
+
+
+/*
+    CRA랑 Vite의 주소처리가 다르다
+    설정이 바뀔 때 마다 img의 경로를 바꿀 수 없기에 조금 동적으로
+
+    CRA : <img src={process.env.PUBLIC_URL + '/images/food1.jpg'}/> // 기본 경로를 따라가준다.
+        기본설정 package.json 파일 뒤에 추가 "homepage":"/detailServicePath" 상세경로설정
+        -> https://localhost:5173/detailServicePath/images/food1.jpg 가 됨.
+    Vite : <img src={ import.meta.env.BASE_URL + '/image/food1.jpg'}/> 
+*/
+function FoodCard(props){
+    let foodsData = props.arr;
+    let index = props.idx;
+    return(
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" src={import.meta.env.BASE_URL + foodsData[index].imgPath}/>
+            <Card.Body>
+                <Card.Title>{foodsData[index].title}</Card.Title>
+                <Card.Text>{foodsData[index].content}</Card.Text>
+                <Card.Text>{foodsData[index].price+"원"}</Card.Text>
+                <Button variant="primary">상세 보기</Button>
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default FoodCard;
