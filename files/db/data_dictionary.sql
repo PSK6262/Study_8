573833e36631c274361bc03f1b31054ddee503f0
PSK6262 <qkrxotn60@gmail.com>
2026-07-21 10:49:24 +0900

    Data Dictionary

---
+++ files/db/data_dictionary.sql
@@ -0,0 +1,18 @@
+--Data Dictionary
+
+select * from tab
+where tname like 'EMP%';
+
+select * from user_tables;
+select * from ALL_tables;
+select * from DBA_tables;
+
+select * from user_views;
+select * from user_sequences;
+select * from user_objects;
+select * from user_constraints;
+select * from user_cons_columns;
+select * from user_indexes;
+select * from user_ind_columns;
+
+select * 
