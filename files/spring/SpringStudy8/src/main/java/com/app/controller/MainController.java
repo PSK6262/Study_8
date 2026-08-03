ae6c8a4ceecb4f76b463779cb32d432bda5f8c2b
PSK6262 <qkrxotn60@gmail.com>
2026-08-03 10:42:16 +0900

    spring quiz01 해답

--- files/spring/SpringStudy8/src/main/java/com/app/controller/MainController.java
+++ files/spring/SpringStudy8/src/main/java/com/app/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping("/main")
+	@RequestMapping("/")
 	public String main() {
 		// view 자원 이름 return
 		return "main";
