2f91dc2f32673cdd48b70ed11665af7cc2f8fb3d
PSK6262 <qkrxotn60@gmail.com>
2026-08-06 09:11:50 +0900

    휴가날 진도나간것 받아오기 (jstl)

--- files/spring/SpringStudy8/src/main/java/com/app/dto/study/request/Product.java
+++ files/spring/SpringStudy8/src/main/java/com/app/dto/study/request/Product.java
@@ -4,9 +4,18 @@ import lombok.Data;
 
 @Data
 public class Product {
+	public Product(String id, String name, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	public Product() {}
 	private String id;
 	private String name;
 	private int price;
+	
+	
 }
 
 //@Getter
