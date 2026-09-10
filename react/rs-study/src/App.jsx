66de1a48da1996502ab3fcf1c716e9102bbd25d5
PSK6262 <qkrxotn60@gmail.com>
2026-09-10 10:25:58 +0900

    [React Spring] 연동확인용 프로젝트 생성 , axios 설치 , proxy 서버 설정 , 서버 REST API 통신

---
+++ react/rs-study/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import AppRS from './pages/AppRS'
+
+
+function App() {
+  return (
+    <>
+      <AppRS/>
+    </>
+  )
+}
+
+export default App
